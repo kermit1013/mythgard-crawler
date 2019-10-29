1e7267275dd3e01194e70d56314fe7f21dce2b20
Kermit Song <kermit.song@qbs.dev>
2019-10-29 23:56:41 +0800

    finished download img

---
+++ index.js
@@ -0,0 +1,65 @@
+const https = require('https');
+const fs = require('fs');
+const cheerio = require('cheerio')
+
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+var driver = new webdriver.Builder()
+  .withCapabilities(webdriver.Capabilities.chrome())
+  .build();
+
+var linkArr = [];
+var pageSource = "";
+//promise
+var getLinks = new Promise(async function (resolve, reject) {
+  const Url = 'https://mythdb.eu/html/cards.html'
+  //async await
+  try {
+    await driver.get(Url)
+    await driver.sleep(10000);
+    await driver.getPageSource().then((res) => {
+      pageSource = res;
+    });
+  } finally {
+    console.log("====get html====")
+    resolve();
+  }
+})
+  .then(() => {
+    console.log('===then===');
+    const $ = cheerio.load(pageSource);
+    $('#cards a').each((index, value) => {
+      var imageLink = $(value).attr('href');
+      imageLinkArr.push(imageLink);
+    });
+    console.log(imageLinkArr);
+  })
+  .then(() => {
+    console.log('===download===');
+    downloadFile();
+    console.log('===quit!===');
+    driver.quit();
+  });
+
+var downloadFile = () => {
+  readFile(function () {
+    console.log("===reading finished!===");
+  });
+}
+// call back function
+var readFile = function (callback) {
+  if (linkArr.length > 0) {
+    var link = linkArr.shift();
+    var fileName = link.split('/')[link.split('/').length - 1];
+    console.log(split[split.length - 1]);
+    var file = fs.createWriteStream(`./cards/${fileName}`);
+    https.get("https://mythdb.eu".concat(link), function (response) {
+      response.pipe(file);
+      readFile(callback);
+    });
+  } else {
+    callback();
+  }
+}  
